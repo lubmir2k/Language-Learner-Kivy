213d0c5736b384f63bdcb3fbf5eade6dd0a29e47
lubmir2k <lubomir.mitana@gmail.com>
2021-04-06 17:28:18 +0200

    Funky button object oriented

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from kivy.uix.button import Button
 
 
 class FunkyButton(Button):
-    pass
+    def __init__(self, **kwargs):
+        super(FunkyButton, self).__init__(**kwargs)
+        self.text = "Funky button"
+        self.pos = (100, 100)
+        self.size_hint = (.5, .5)
 
 
 class LanguageLearnerApp(App):
     def build(self):
-        return FunkyButton(
-            text="Funky button",
-            pos=(100, 100),
-            size_hint=(.5, .5)
-        )
+        return FunkyButton()
 
 
 if __name__ == '__main__':
