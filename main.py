b16011955d71e90b213c97d4347e771f089109b3
lubmir2k <lubomir.mitana@gmail.com>
2021-04-06 17:18:14 +0200

    Added a button

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from kivy.uix.button import Button
 
 class LanguageLearnerApp(App):
     def build(self):
-        return Button(text='Hello World')
+        return Button(
+            text='Hello World',
+            pos=(50, 50),
+            size=(100, 100),
+            size_hint=(None,None)
+        )
 
 
 if __name__ == '__main__':
