6d1773b616d2507aa6821d37b25af7ec13706fbd
lubmir2k <lubomir.mitana@gmail.com>
2021-04-07 20:30:55 +0200

    Used .kv file for the first time

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from kivy.uix.button import Button
 
 
 class FunkyButton(Button):
-    def __init__(self, **kwargs):
-        super(FunkyButton, self).__init__(**kwargs)
-        self.text = "Funky button"
-        self.pos = (100, 100)
-        self.size_hint = (.5, .5)
+    pass
 
 
 class LanguageLearnerApp(App):
     def build(self):
-        return FunkyButton()
+        return FunkyButton(
+            pos=(100, 100),
+            size_hint=(None, None),
+            size=(500, 500)
+        )
 
 
 if __name__ == '__main__':
